f2cb69853dacd914120264c767c11b2b54397e19
Gustavo Silva <126671246+joaogstex@users.noreply.github.com>
2024-05-30 21:00:51 -0300

    atualizando whatsapp

--- src/main/java/com/ucsal/notification/channels/WhatsAppNotification.java
+++ src/main/java/com/ucsal/notification/channels/WhatsAppNotification.java
@@ -8,19 +8,21 @@ import com.ucsal.notification.interfaces.NotificationChannel;
 import com.ucsal.notification.models.Notification;
 
 @NoticationChannelType("whatsapp")
-public class WhatsAppNotification implements NotificationChannel{
-	 public static final String ACCOUNT_SID = "SID";
-	 public static final String AUTH_TOKEN = "TOKEN";
-	 
-	 static {
-		 Twilio.init(AUTH_TOKEN, ACCOUNT_SID);
-	 }
-	 
-	@Override
-	public void send(Notification notification) {
-		Message message = Message.creator(new PhoneNumber("whatsapp:+yournumber"),new PhoneNumber("whatsapp:+"+notification.getRecipient()),notification.getMessage()).create();
-		
-		 System.out.println("Sent WhatsApp message successfully: " + message.getSid());
-	}
+public class WhatsAppNotification implements NotificationChannel {
+    public static final String ACCOUNT_SID = "AC806926aa89e2a6d24f6063d9409cad15";
+    public static final String AUTH_TOKEN = "c0f66032c721c08653be0076c38618bf";
 
+    static {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+    }
+
+    @Override
+    public void send(Notification notification) {
+        Message message = Message.creator(
+                new PhoneNumber("whatsapp:" + notification.getRecipient()),
+                new PhoneNumber("whatsapp:+14155238886"),
+                notification.getMessage()).create();
+        
+        System.out.println("Notificação Whatsapp enviada com sucesso: " + message.getSid());
+    }
 }
